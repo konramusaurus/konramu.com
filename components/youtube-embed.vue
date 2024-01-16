248c8f9514626e311165cd9b8f9bb93542169d95
こんらむザウルス <156505487+konramusaurus@users.noreply.github.com>
2024-01-16 16:30:25 +0000

    twitter to x

--- components/youtube-embed.vue
+++ components/youtube-embed.vue
@@ -13,12 +13,11 @@
       <span class="mt-4 inline-flex">
         <a href="https://twitter.com/slime_ramu" target="_blank" rel="noopener noreferrer">
           <button type="button"
-            class="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/70 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-full text-sm px-2 py-2 text-center inline-flex items-center mr-2 mb-2">
-            <svg class="w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter" role="img"
-              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path fill="currentColor"
-                d="M459.4 151.7c.325 4.548 .325 9.097 .325 13.65 0 138.7-105.6 298.6-298.6 298.6-59.45 0-114.7-17.22-161.1-47.11 8.447 .974 16.57 1.299 25.34 1.299 49.06 0 94.21-16.57 130.3-44.83-46.13-.975-84.79-31.19-98.11-72.77 6.498 .974 12.99 1.624 19.82 1.624 9.421 0 18.84-1.3 27.61-3.573-48.08-9.747-84.14-51.98-84.14-102.1v-1.299c13.97 7.797 30.21 12.67 47.43 13.32-28.26-18.84-46.78-51.01-46.78-87.39 0-19.49 5.197-37.36 14.29-52.95 51.65 63.67 129.3 105.3 216.4 109.8-1.624-7.797-2.599-15.92-2.599-24.04 0-57.83 46.78-104.9 104.9-104.9 30.21 0 57.5 12.67 76.67 33.14 23.72-4.548 46.46-13.32 66.6-25.34-7.798 24.37-24.37 44.83-46.13 57.83 21.12-2.273 41.58-8.122 60.43-16.24-14.29 20.79-32.16 39.31-52.63 54.25z">
-              </path>
+            class="text-white bg-[#eee] hover:bg-[#ddd]/70 focus:ring-4 focus:outline-none focus:ring-[#ccc]/50 font-medium rounded-full text-sm px-2 py-2 text-center inline-flex items-center mr-2 mb-2">
+            <svg class="w-4 h-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <g>
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+              </g>
             </svg>
           </button>
         </a>
