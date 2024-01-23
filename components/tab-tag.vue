d4255bdd81e4ba12a79de8cd7bb95295e8d802ad
こんらむザウルス <156505487+konramusaurus@users.noreply.github.com>
2024-01-23 17:12:32 +0000

    tag filter worked

--- components/tab-tag.vue
+++ components/tab-tag.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="mx-auto mt-5 flex flex-wrap">
     <div v-for="(voice, index) in taggedVoices()" :key="index" class="w-full md:w-1/2 px-2 text-center">
-      <ButtonVoices :text="voice.text" :voice="voice.voice" :tags="voice.tags" />
+      <ButtonVoices :slug="voice.slug" :voice_url="voice.voice_url" :tags="voice.tags" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-// import { voices } from '@/voices/index';
+import type { VoiceMap } from '@/types/index';
 
 interface Props {
+  voices: VoiceMap[];
   tag: string;
 }
 const Props = withDefaults(defineProps<Props>(), {
   tag: '',
 });
 
-// const taggedVoices = () => {
-//   return voices.flatMap(v => v.voices).filter(v => v.tags.includes(Props.tag));
-// }
+const taggedVoices = () => {
+  return Props.voices.flatMap(v => v.voices).filter(v => v.tags.includes(Props.tag));
+}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
   <div id="view-tab" class="w-full">
     <div class="border-b border-gray-200 mb-4">
       <ul class="flex flex-wrap -mb-px">
-        <li class="mr-2" v-for="tab of ['all', 'konramu', 'kawaii', 'kawaiso', 'nakigoe', 'zettaiousei', 'lewd']">
+        <li class="mr-2" v-for="tab of ['all', 'konramu', 'kawaii', 'kawaiso', 'nakigoe']">
           <button @click="toggleTab(tab)" :class="{'selectedTab': activeTab === tab}" class="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2" type="button" >{{ tab === 'lewd' ? '...' : tab[0].toUpperCase() + tab.slice(1).toLowerCase() }}</button>
         </li>
       </ul>
@@ -18,8 +18,7 @@
         <TabAll :voices="voices" />
       </div>
       <div v-else>
-        <TabAll :voices="voices" />
-        <!--TabTag :tag="activeTab" /-->
+        <TabTag :tag="activeTab" :voices="voices" />
       </div>
     </div>
   </div>
@@ -44,7 +43,13 @@ const { data: voices, pending, error } = useFetch<VoiceMap[]>(url, {
   initialData: [],
   transform: (response) => {
     try {
-      return JSON.parse(response);
+      const parsed = JSON.parse(response);
+      return parsed.sort((a, b) => {
+        const formatDate = (dateStr) => {
+          return dateStr.replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3');
+        };
+        return new Date(formatDate(b.publish_date)) - new Date(formatDate(a.publish_date));
+      });
     } catch (e) {
       console.error('JSON parsing error:', e);
       return null;
