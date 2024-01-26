d90cbce2c146b0f63e1671edfe95cc1129777ce9
こんらむザウルス <156505487+konramusaurus@users.noreply.github.com>
2024-01-26 12:55:33 +0000

    delete baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/konramu.com/',
-  },
   target: 'static',
   css: ['~/assets/css/main.css'],
   postcss: {
