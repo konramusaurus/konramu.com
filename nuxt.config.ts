fc9fef3ad7d0a6e4360a30b87ba41b2124397d92
こんらむザウルス <156505487+konramusaurus@users.noreply.github.com>
2024-01-26 12:32:26 +0000

    update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: 'node-server',
-  },
-  ssr: true,
   target: 'static',
   css: ['~/assets/css/main.css'],
   postcss: {
