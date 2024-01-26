86e2a92cb99df7cfccfbeb2ff7c03a427a0bd903
こんらむザウルス <156505487+konramusaurus@users.noreply.github.com>
2024-01-26 12:42:28 +0000

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/konramu.com/',
+  },
   target: 'static',
   css: ['~/assets/css/main.css'],
   postcss: {
