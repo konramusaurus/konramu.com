d7224db30b4be371e51bb94e6c488c633d60c181
こんらむザウルス <156505487+konramusaurus@users.noreply.github.com>
2024-02-17 22:31:01 +0000

    change heading text

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
     <div class="container px-5 py-6 mx-auto">
       <div class="flex flex-col text-center w-full mb-12">
         <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">こんらむ～</h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">ﾎﾞｲｽ ﾏｯﾃﾙﾖ</p>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">ﾊﾞﾚﾝﾀｲﾝ ﾎﾞｲｽ ｵｽｽﾒ</p>
       </div>
       <div class="lg:w-2/3 md:w-7/8 mx-auto">
         <div class="flex flex-wrap -m-2">
