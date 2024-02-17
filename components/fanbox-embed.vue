695d77c5d6534664a4ff000f04326a6f55c8b5a6
こんらむザウルス <156505487+konramusaurus@users.noreply.github.com>
2024-02-17 22:30:49 +0000

    bugfix hyperref

--- components/fanbox-embed.vue
+++ components/fanbox-embed.vue
@@ -6,7 +6,7 @@
       <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
         <span class="mr-1 font-semibold">2023.12.03</span>3歳の誕生日 & 歌枠 🍰<br>
         <span>
-          <a href="https://www.youtube.com/watch?v=FIhZ13BQJeQ" class="inline-flex items-center text-pink-400 hover:underline">
+          <a href="https://www.youtube.com/watch?v=FIhZ13BQJeQ" class="inline-flex items-center text-pink-400 hover:underline" target="_blank" rel="noopener noreferrer">
             【雑談】誕生日スライム！今年が終わってしまうので歌も歌う…！【歌枠】
             <svg class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
           </a>
@@ -16,7 +16,7 @@
       <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
         <span class="mr-1 font-semibold">2023.09.04</span> 登録者1,000人突破 & 収益化🎉<br>
         <span>
-          <a href="https://www.youtube.com/watch?v=LaLJS8vysro" class="inline-flex items-center text-pink-400 hover:underline">
+          <a href="https://www.youtube.com/watch?v=LaLJS8vysro" class="inline-flex items-center text-pink-400 hover:underline" target="_blank" rel="noopener noreferrer">
             【記念配信】登録者1000人＆収益化ありがとうございます！【雑談＆月見を食べるよ】
             <svg class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
           </a>
@@ -26,7 +26,7 @@
       <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
         <span class="mr-1 font-semibold">2023.05.26</span> PIXIV FANBOX開設🍎<br>
         <span>
-          <a href="https://slime-ramu.fanbox.cc/" class="inline-flex items-center text-pink-400 hover:underline">
+          <a href="https://slime-ramu.fanbox.cc/" class="inline-flex items-center text-pink-400 hover:underline" target="_blank" rel="noopener noreferrer">
             【新人Vtuber】らむ｜pixivFANBOX
             <svg class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
           </a>
